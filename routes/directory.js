e01e1c4a2863847f406450d1228bb3966db489dd
Rexagon <reide740@gmail.com>
2016-05-14 21:39:55 +0300

    Created warehouses table in directory

--- routes/directory.js
+++ routes/directory.js
@@ -24,23 +24,34 @@ router.get('/directory', app.restrict, app.restrictPage, function(req, res) {
 				});
 			});
 
-			res.render('template', {
-				authData: authData,
-				title: 'Справочник',
-				styles: [
-					'assets/global/plugins/datatables/datatables.min.css',
-					'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css'
-				],
-				scripts: [
-					'assets/global/scripts/datatable.js',
-					'assets/global/plugins/datatables/datatables.min.js',
-					'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js',
-					'scripts/directory.js'
-				],
-				page: 'directory',
-				section: 'directory',
-				countries: countries,
-				appAreas: appAreas
+			fm.ref().child('directory/warehouses').once('value', function(snapshotWarehouses) {
+				var warehouses = [];
+				snapshotWarehouses.forEach(function(data) {
+					warehouses.push({
+						id: data.key(),
+						name: data.val().name
+					});
+				});
+
+				res.render('template', {
+					authData: authData,
+					title: 'Справочник',
+					styles: [
+						'assets/global/plugins/datatables/datatables.min.css',
+						'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css'
+					],
+					scripts: [
+						'assets/global/scripts/datatable.js',
+						'assets/global/plugins/datatables/datatables.min.js',
+						'assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js',
+						'scripts/directory.js'
+					],
+					page: 'directory',
+					section: 'directory',
+					countries: countries,
+					appAreas: appAreas,
+					warehouses: warehouses
+				});
 			});
 		});
 	});
@@ -79,6 +90,42 @@ router.post('/directory/countries/edit', app.restrict, app.restrictPage, functio
 router.post('/directory/countries/delete', app.restrict, app.restrictPage, function(req, res) {
 	fm.ref().child('directory/countries/' + req.body.id).remove(function(error) {
 		res.send('Страна успешно удалена');
+	});
+});
+
+//WAREHOUSES
+router.post('/directory/warehouses/edit', app.restrict, app.restrictPage, function(req, res) {
+	fm.ref().child('directory/warehouses').orderByChild('name').equalTo(req.body.name).once('value', function(snapshot) {
+		var result = {
+			text: '',
+			id: ''
+		};
+		if (snapshot.exists()) {
+			result.text = 'Склад с таким именем уже существует!';
+			res.send(result);
+		} else {
+			if (req.body.isNew == 'true') {
+				var idRef = fm.ref().child('directory/warehouses').push({
+					name: req.body.name
+				}, function(error) {
+					result.id = idRef.key();
+					res.send(result);
+				});
+			} else {
+				fm.ref().child('directory/warehouses/' + req.body.id).set({
+					name: req.body.name
+				}, function(error) {
+					result.id = req.body.id;
+					res.send(result);
+				});
+			}
+		}
+	});
+});
+
+router.post('/directory/warehouses/delete', app.restrict, app.restrictPage, function(req, res) {
+	fm.ref().child('directory/warehouses/' + req.body.id).remove(function(error) {
+		res.send('Склад успешно удален');
 	});
 });
 
